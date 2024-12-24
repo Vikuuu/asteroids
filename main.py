b6df68afe19cecd885ee3f6be44b3dea5ebb6895
Vikuuu <adivik672@gmail.com>
2024-12-24 11:56:47 +0530

    feat(shoot): Add bullet shoot

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
@@ -14,12 +15,17 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
-    asteroid_field = AsteroidField()
+    Player.containers = (
+        updatable,
+        drawable,
+    )
+    Shot.containers = (shots, updatable, drawable)
 
-    Player.containers = (updatable, drawable)
+    asteroid_field = AsteroidField()
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
